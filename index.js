c9ee87d4eeec7b00ff3b3eedc09aab8897635361
Chris62186 <81889195+Chris62186@users.noreply.github.com>
2021-08-31 21:46:56 -0400

    Launchpad 2.0
    Full overhaul of site

--- index.js
+++ index.js
@@ -6,39 +6,29 @@ var time = d.getHours();
 console.log(time);
 
 if (time < 12) {
-  document.querySelector(".header-bar-text").innerHTML = "Good morning, Christopher!"
+  document.querySelector(".welcome-message").innerHTML = "Good morning, Christopher!"
 } else if (time >= 12 && time < 17){
-  document.querySelector(".header-bar-text").innerHTML = "Good afternoon, Christopher!"
+  document.querySelector(".welcome-message").innerHTML = "Good afternoon, Christopher!"
 } else {
-  document.querySelector(".header-bar-text").innerHTML = "Time to shut it down, Christopher!"
+  document.querySelector(".welcome-message").innerHTML = "Time to shut it down, Christopher!"
 }
 
-// Countdown Timer
-// Runs every second
+// Initial run of timer
+timer();
+
+
+// Countdown Timer, Runs every second
 setInterval(function() {
-  var now = new Date();
-  var hoursLeft = 16 - now.getHours();
-  var minutesLeft = 59 - now.getMinutes();
-  var secondsLeft = 59 - now.getSeconds();
-
-
-  if (now.getHours() < 8) {
-    document.querySelector(".countdown-timer").innerHTML = "Not 8am yet - No work!";
-  } else if (now.getHours() >= 17) {
-    document.querySelector(".countdown-timer").innerHTML = "You made it to 5PM! Be done.";
-  } else {
-    document.querySelector(".countdown-timer").innerHTML = "Time until 5PM: " + hoursLeft + ":" + minutesLeft + ":" + secondsLeft;
-  }
+  timer();
 }, 1000);
 
 
 // Button Color Randomizer
-
 document.querySelector(".btn-color-randomizer").addEventListener("click", function() {
   buttonColorRandomizer();
 });
 
-buttonColorRandomizer();
+
 
 
 
@@ -55,4 +45,20 @@ function buttonColorRandomizer() {
 
 function randomColors() {
   return '#' + Math.floor(Math.random() * 16777215).toString(16);
+}
+
+function timer() {
+  var now = new Date();
+  var hoursLeft = 16 - now.getHours();
+  var minutesLeft = 59 - now.getMinutes();
+  var secondsLeft = 59 - now.getSeconds();
+
+
+  if (now.getHours() < 8) {
+    document.querySelector(".countdown-timer").innerHTML = "Not 8am yet - No work!";
+  } else if (now.getHours() >= 17) {
+    document.querySelector(".countdown-timer").innerHTML = "You made it to 5PM! Be done.";
+  } else {
+    document.querySelector(".countdown-timer").innerHTML =  hoursLeft + ":" + minutesLeft + ":" + secondsLeft;
+  }
 }
